a4c1e8c92ad621235deaa1f268696d73d2d73020
kejlor <woj450@gmail.com>
2023-05-12 14:35:17 +0200

    init commit

---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Bartosz Wojtkowiak on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
