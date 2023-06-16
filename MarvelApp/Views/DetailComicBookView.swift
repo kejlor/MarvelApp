d19d9be6df372d142cf85e54c56535ef712b283d
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-06-16 13:15:56 +0200

    Add enums with heights, widths etc. for views

--- MarvelApp/Views/DetailComicBookView.swift
+++ MarvelApp/Views/DetailComicBookView.swift
@@ -14,20 +14,20 @@ struct DetailComicBookView: View {
     
     var body: some View {
         VStack() {
-                AsyncImage(url: URL(string: "\(comicVM.thumbnailPath).jpg")) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: UIScreen.main.bounds.size.height - 375)
-                } placeholder: {
-                    ProgressView()
-                }
-        Spacer()
+            AsyncImage(url: URL(string: "\(comicVM.thumbnailPath).jpg")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: UIScreen.main.bounds.size.height - DetailComicBookSheetHeights.spaceBetweenImageAndSheet.rawValue)
+            } placeholder: {
+                ProgressView()
+            }
+            Spacer()
         }
         .sheet(isPresented: $showingCredits) {
             ComicSheetView(comicVM: comicVM)
                 .padding()
-                .presentationDetents([.height(275), .medium])
+                .presentationDetents([.height(DetailComicBookSheetHeights.minimalSheetHeight.rawValue), .medium])
                 .onDisappear {
                     dismiss()
                 }
@@ -39,4 +39,9 @@ struct DetailComicBookView_Previews: PreviewProvider {
     static var previews: some View {
         DetailComicBookView(comicVM: ComicViewModel(comic: Comic(id: 1, title: "Avengers #39", description: "Re-live the legendary first journey into the dystopian future of 2013 - where Sentinels stalk the Earth, and the X-Men are humanity's only hope...until they die! Also featuring the first appearance of Alpha Flight, the return of the Wendigo, the history of the X-Men from Cyclops himself...and a demon for Christmas!? ", thumbnail: Thumbnail(path: "http://i.annihil.us/u/prod/marvel/i/mg/9/d0/58b5cfb6d5239"), creators: CreatorResponse(items: [Creator(name: "JJ Abrams"), Creator(name: "Allan"), Creator(name: "John Doe")]), urls: [UrlResponse(type: "detail", url: "https://www.marvel.com/comics/issue/183/startling_stories_the_incorrigible_hulk_2004_1?utm_campaign=apiRef&utm_source=080a502746c8a60aeab043387a56eef0")])))
     }
+}
+
+private enum DetailComicBookSheetHeights: CGFloat {
+    case minimalSheetHeight = 275
+    case spaceBetweenImageAndSheet = 375
 }
