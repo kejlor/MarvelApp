f60e1757f2714879eb00bbd38dc6ce86f1cbca22
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-06-21 09:49:14 +0200

    Add unit tests

--- MarvelAppUnitTests/UnitTests/ComicsRepositoryUnitTests.swift
+++ MarvelAppUnitTests/UnitTests/ComicsRepositoryUnitTests.swift
@@ -14,11 +14,23 @@ class ComicsRepositoryUnitTests: XCTestCase {
     func test_should_not_get_any_images() async {
         let comicsRepository = ComicsRepository(networkService: MockNetworkService())
         let fetchedImage = try? await comicsRepository.getImage(from: "")
-        XCTAssertEqual(fetchedImage, nil)
+        XCTAssertNil(fetchedImage)
     }
     
     func test_urlString_should_return_value() async {
         let comicsRepository = MockComicsRepository(networkService: MockNetworkService())
         XCTAssertEqual("Random string", comicsRepository.urlString)
+    }
+    
+    func test_fetchData_should_not_return_decoded_data() async {
+        let networkService = MockNetworkService()
+        let decodedType = try? await networkService.fetchData() as ComicsResponse
+        XCTAssertNil(decodedType)
+    }
+    
+    func test_fetchImage_should_return_data() async {
+        let networkService = MockNetworkService()
+        let image = try? await networkService.fetchImage(url: "")
+        XCTAssertEqual(Data().self, image.self)
     }
 }
