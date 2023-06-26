8744b4069b9c44011b8fcfa2e17d5a09e16cd7d8
kejlor <bartoszwojtkowiak.dev@gmail.com>
2023-06-26 12:55:46 +0200

    Fixed navigation

--- MarvelApp/Components/ComicListView.swift
+++ MarvelApp/Components/ComicListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ComicListView: View {
     @EnvironmentObject var vm: ComicListViewModel
     @ObservedObject var scannerVM = ScannerViewModel()
-    @State private var navPath = NavigationPath()
+    @State private var navPath = [ComicViewModel]()
     var comics: [ComicViewModel]
     
     var body: some View {
@@ -40,7 +40,7 @@ struct ComicListView: View {
                 ScannerView()
                     .onDisappear {
 //                        navPath.append(scannerVM.returnFetchedComics())
-                        navPath.append(scannerVM.fetchedComics.first)
+                        navPath.append(scannerVM.fetchedComics.first ?? ComicViewModel(comic: Comic(id: 1, title: "Avengers #39", description: "Re-live the legendary first journey into the dystopian future of 2013 - where Sentinels stalk the Earth, and the X-Men are humanity's only hope...until they die! Also featuring the first appearance of Alpha Flight, the return of the Wendigo, the history of the X-Men from Cyclops himself...and a demon for Christmas!? ", thumbnail: Thumbnail(path: "http://i.annihil.us/u/prod/marvel/i/mg/9/d0/58b5cfb6d5239"), creators: CreatorResponse(items: [Creator(name: "JJ Abrams"), Creator(name: "Allan"), Creator(name: "John Doe")]), urls: [UrlResponse(type: "detail", url: "https://www.marvel.com/comics/issue/183/startling_stories_the_incorrigible_hulk_2004_1?utm_campaign=apiRef&utm_source=080a502746c8a60aeab043387a56eef0")])))
 //                        print(navPath)
                     }
             }
